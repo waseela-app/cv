4ed06a0818b169ea4940f98ee576d25361769e85
Mohamed <d2105944@hotmail.com>
2022-09-11 05:35:27 +0300

    first commit

---
+++ src/components/Bottom.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Bottom() {
+    return (
+        <div className='bottom section'>
+            <div className='reveal'>
+                <h1 className='text-center py-5' style={{color: 'rgb(108, 0, 196)'}}>Contact</h1>
+                <h2>
+                    E-Mail: d2105944@gmail.com<br/><br/>
+                    Twitter: <a target='_blank' style={{color: 'white'}} href="https://twitter.com/waseela2k">@Waseela2k</a><br/><br/>
+                    Linked in: <a target='_blank' style={{color: 'white'}} href="https://www.linkedin.com/in/mohamed-alimam-474b9522a/">Mohamed Alemam</a><br/><br/>
+                    call + Whats app + Telegram: <span style={{textDecoration: 'underline'}}>+966576344747</span>
+                </h2>
+            </div>
+        </div>
+    )
+}
